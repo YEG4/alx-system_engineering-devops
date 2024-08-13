ae0dfdb4fe4b4e8d41730fa9127f901fc1966e95
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-08-13 19:15:10 +0300

    U

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,8 @@
 """This module sends a request
 to the Reddit API and returns the number of subscribers
 """
-import json
 import requests
 import sys
-
-import requests.api
 
 
 def number_of_subscribers(subreddit):
@@ -15,14 +12,10 @@ def number_of_subscribers(subreddit):
     Args:
         subreddit (string): path to subreddit to get subscribers
     """
-
-    if len(sys.argv) < 2:
+    res = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                       headers={'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}, allow_redirects=False)
+    if res.status_code == 404:
         return 0
-    else:
-        res = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                           headers={'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}, allow_redirects=False)
-        if res.status_code == 404:
-            return 0
-        res = res.json().get("data").get("subscribers")
-        print(res)
-        return res
+    res = res.json().get("data").get("subscribers")
+    print(res)
+    return res
