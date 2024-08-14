c337bc67ff869c8994b712f7fcb77c60a911399b
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-08-14 09:56:23 +0300

    U

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,10 +4,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Return the total number of subscribers on a given subreddit
+
+    Args:
+        subreddit (str): path name to the subreddit
+
+    Returns:
+        int: number of subscribers or 0 if invalid subreddit
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
